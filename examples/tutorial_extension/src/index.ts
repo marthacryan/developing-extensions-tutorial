a4a2b2c835e6d64b9b2f5b98b8bccc6f05573d74
Alex Bozarth <ajbozart@us.ibm.com>
2022-07-10 20:51:54 -0500

    add exercise and lint

--- examples/tutorial_extension/src/index.ts
+++ examples/tutorial_extension/src/index.ts
@@ -5,7 +5,7 @@ import {
 import { MainAreaWidget } from '@jupyterlab/apputils';
 import { ILauncher } from '@jupyterlab/launcher';
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
-import { jupyterIcon } from '@jupyterlab/ui-components';
+import { imageIcon } from '@jupyterlab/ui-components';
 
 import { TutorialWidget } from './widget';
 
@@ -16,7 +16,11 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'tutorial-extension:plugin',
   autoStart: true,
   optional: [ISettingRegistry, ILauncher],
-  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null, launcher: ILauncher | null) => {
+  activate: (
+    app: JupyterFrontEnd,
+    settingRegistry: ISettingRegistry | null,
+    launcher: ILauncher | null
+  ) => {
     console.log('JupyterLab extension tutorial-extension is activated!');
 
     // Load the settings from schema/plugin.json
@@ -25,10 +29,16 @@ const plugin: JupyterFrontEndPlugin<void> = {
       settingRegistry
         .load(plugin.id)
         .then(settings => {
-          console.log('tutorial-extension settings loaded:', settings.composite);
+          console.log(
+            'tutorial-extension settings loaded:',
+            settings.composite
+          );
         })
         .catch(reason => {
-          console.error('Failed to load settings for tutorial-extension.', reason);
+          console.error(
+            'Failed to load settings for tutorial-extension.',
+            reason
+          );
         });
     }
 
@@ -38,12 +48,14 @@ const plugin: JupyterFrontEndPlugin<void> = {
         const widget = new TutorialWidget();
         const main = new MainAreaWidget({ content: widget });
         main.title.label = 'Tutorial Widget';
-        main.title.icon = jupyterIcon;
+        main.title.icon = imageIcon;
         main.title.caption = widget.title.label;
+
+        // TODO: add a button to refresh image
 
         app.shell.add(main, 'main');
       },
-      icon: jupyterIcon,
+      icon: imageIcon,
       label: 'Open Tutorial Widget'
     });
 
--- examples/tutorial_extension/src/widget.ts
+++ examples/tutorial_extension/src/widget.ts
@@ -1,22 +1,24 @@
-import { Widget } from "@lumino/widgets";
-import { requestAPI } from "./handler";
+import { Widget } from '@lumino/widgets';
+import { requestAPI } from './handler';
 
 export class TutorialWidget extends Widget {
   constructor() {
     super();
     this.img = document.createElement('img');
-    this.img.src = ''
+    // hard code image source
+    this.img.src = 'https://picsum.photos/600/400';
     this.node.appendChild(this.img);
-    this.load_image()
+    // retrieve image from API
+    // this.load_image()
   }
 
-  load_image() {
+  load_image(): void {
     // This is an example API call to the server extension associated with
     // this jupyterlab extension. It uses the generated handler.ts utility
     requestAPI<any>('image')
       .then(data => {
         console.log(data);
-        this.img.src = data.image_url
+        this.img.src = data.image_url;
       })
       .catch(reason => {
         console.error(
